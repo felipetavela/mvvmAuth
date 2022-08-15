8a25d7e2eee12e7ae0c99753dab3906f3832c7b8
Felipe Tavela <felipe.tavela@hotmail.com>
2022-08-15 17:46:31 -0300

    Initial Commit

---
+++ mvvmAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvvmAuth
+//
+//  Created by Felipe Tavela on 15/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
